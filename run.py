c79dc1a4c83a6c427b33ef8a441727666731da09
mcalik6 <mcalik6@gmail.com>
2023-04-24 18:00:41 +0000

    data collected

--- run.py
+++ run.py
@@ -18,9 +18,9 @@ def get_name ():
     function is getting name of the user
     """
     print ("Thank you for contacting our service team\n")
-    name_str = input("Enter Your name here \n")
+    print ("This is PC related departement\n")
+    name_str = input("Please provide your name \n")
     return name_str
-
 
 def update_name_worksheet(name):
     """
@@ -29,6 +29,48 @@ def update_name_worksheet(name):
     print("thank you for your name...\n")
     name_worksheet = SHEET.worksheet("name")
     name_worksheet.append_row([name])
-  
-name = get_name()
-update_name_worksheet(name)
+
+def get_surname ():
+    """
+    function is getting surname of the user
+    """
+    surname_str = input("Please provide your Surname \n")
+    return surname_str
+    
+def update_surname_worksheet(surname):
+    """
+    Update Surname sheet
+    """
+    print("thank you for your surname...\n")
+    surname_worksheet = SHEET.worksheet("surname")
+    surname_worksheet.append_row([surname])
+
+def get_phone ():
+    """
+    function is getting phone number of the user
+    """
+    while True:
+        try:
+            number = int(input ("Enter Your phone number: \n"))
+            return number
+        except ValueError:
+            print ("Invalid input, Please enter phone number")
+    
+def update_phone_worksheet(phone):
+    """
+    Update Phone number sheet
+    """
+    print("thank you for your Phone number...\n")
+    phone_worksheet = SHEET.worksheet("phone_number")
+    phone_worksheet.append_row([phone])
+
+def main():
+
+    name = get_name()
+    update_name_worksheet(name)
+    surname = get_surname()
+    update_surname_worksheet(surname)
+    phone = get_phone()
+    update_phone_worksheet(phone)
+
+main ()
